dff769b6e3ef0109c6371618581822bbb133614e
Theo Foucher <theonicolas.foucher@gmail.com>
2024-02-28 14:51:32 +0100

    footer terminé

--- article.php
+++ article.php
@@ -1,5 +1,16 @@
 <?php
+    include'connexion_base.php';
 
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        
+        // Récupérer les données de l'étudiant avec l'ID spécifié
+        $query = "SELECT * FROM article WHERE id_article = :id";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        $donnee_article = $stmt->fetch(PDO::FETCH_ASSOC);
+    }
 
 
 ?>
@@ -16,6 +27,8 @@
 <header>
     <?php include'header.html';?>
 </header>
-
+<main>
+    <h3 class="titre"></h3>
+</main>
 </body>
 </html>
